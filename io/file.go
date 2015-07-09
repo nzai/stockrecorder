43abf35649db4de5035d590696d18b51d8c55b76
nzai <nzai@163.com>
2015-07-10 00:53:24 +0800

    增加将单字符串写入文件的方法，并在打开文件前确保目录存在

--- io/file.go
+++ io/file.go
@@ -6,21 +6,10 @@ import (
 )
 
 //	写文件
-func WriteFile(filePath string, lines []string) error {
-
-	//	检查文件所处目录是否存在
-	fileDir := filepath.Dir(filePath)
-	_, err := os.Stat(fileDir)
-	if os.IsNotExist(err) {
-		//	如果不存在就先创建目录
-		err = os.Mkdir(fileDir, 0x777)
-		if err != nil {
-			return err
-		}
-	}
+func WriteLines(filePath string, lines []string) error {
 
 	//	打开文件
-	file, err := os.OpenFile(filePath, os.O_CREATE, 0x777)
+	file, err := openFile(filePath)
 	if err != nil {
 		return err
 	}
@@ -36,4 +25,36 @@ func WriteFile(filePath string, lines []string) error {
 	}
 
 	return nil
+}
+
+//	写入字符串
+func WriteString(filePath, content string) error {
+
+	//	打开文件
+	file, err := openFile(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.WriteString(content)
+
+	return nil
+}
+
+//	打开文件
+func openFile(filePath string) (*os.File, error) {
+	//	检查文件所处目录是否存在
+	fileDir := filepath.Dir(filePath)
+	_, err := os.Stat(fileDir)
+	if os.IsNotExist(err) {
+		//	如果不存在就先创建目录
+		err = os.Mkdir(fileDir, 0x777)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	//	打开文件
+	return os.OpenFile(filePath, os.O_CREATE, 0x777)
 }
