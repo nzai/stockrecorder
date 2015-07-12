4d381a0cff2b6cd4e32a87018961eec679bd4f97
nzai <nzai@163.com>
2015-07-12 15:26:15 +0800

    简化了任务管理代码

--- task/task.go
+++ task/task.go
@@ -2,32 +2,20 @@ package task
 
 import (
 	"log"
-	"time"
 
-	"github.com/nzai/stockrecorder/stock"
+	"github.com/nzai/stockrecorder/analyse"
+	"github.com/nzai/stockrecorder/crawl"
 )
 
 //	启动任务
 func StartTasks() error {
 	log.Print("启动任务")
-	channel := make(chan int)
 
-	now := time.Now().UTC()
-	tomorrow := now.AddDate(0, 0, 1)
-	tomorrowZero := tomorrow.Truncate(time.Hour * 24)
+	//	启动抓取任务
+	go crawl.StartJobs()
 
-	d := tomorrowZero.Sub(now)
-	time.AfterFunc(d, func() {
-		ticker := time.NewTicker(time.Hour * 24)
-		for _ = range ticker.C {
-			err := stock.GetToday()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	})
-
-	<-channel
+	//	启动分析任务
+	go analyse.StartJobs()
 
 	return nil
 }
