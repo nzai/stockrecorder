6671f77ce6d88afa7e42c965096462895d4580fd
nzai <nzai@163.com>
2015-07-17 23:55:32 +0800

    增加根据交易结束时间推算所属日期的逻辑

--- crawl/yahoo.go
+++ crawl/yahoo.go
@@ -18,7 +18,7 @@ const (
 )
 
 //	抓取雅虎今日数据
-func yahooToday(market string) error {
+func yahooToday(market string, endHour int) error {
 	//	股票编码
 	codes := config.GetArray(market, "codes")
 	if len(codes) == 0 {
@@ -30,11 +30,11 @@ func yahooToday(market string) error {
 
 	for _, code := range codes {
 		//	并发抓取
-		go func(c string) {
+		go func(c string, h int) {
 
 			for try := 0; try < retryCount; try++ {
 				//	更新每只股票的历史
-				err := yahooTodayStock(c)
+				err := yahooTodayStock(c, h)
 				if err == nil {
 					break
 				}
@@ -45,7 +45,7 @@ func yahooToday(market string) error {
 
 			<-chanSend
 			chanReceive <- 1
-		}(code)
+		}(code, endHour)
 
 		chanSend <- 1
 	}
@@ -62,10 +62,10 @@ func yahooToday(market string) error {
 }
 
 //	抓取雅虎今日股票数据
-func yahooTodayStock(code string) error {
+func yahooTodayStock(code string, endHour int) error {
 
 	//	抓取数据
-	raw, err := io.GetString(yahooQueryUrl(code))
+	raw, err := io.GetString(yahooQueryUrl(code, endHour))
 	if err != nil {
 		return err
 	}
@@ -76,20 +76,27 @@ func yahooTodayStock(code string) error {
 		return err
 	}
 
-	fileName := fmt.Sprintf("%s_raw.txt", time.Now().Format("20060102"))
+	fileName := fmt.Sprintf("%s_raw.txt", getCurrentDate(endHour).Format("20060102"))
 	filePath := filepath.Join(dataDir, code, fileName)
 
 	return io.WriteString(filePath, raw)
 }
 
 //	构造雅虎查询url
-func yahooQueryUrl(code string) string {
+func yahooQueryUrl(code string, endHour int) string {
 
-	now := time.Now()
-	start := now.Truncate(time.Hour * 24)
+	start := getCurrentDate(endHour)
 	end := start.Add(time.Hour * 24)
 
 	pattern := "https://finance-yql.media.yahoo.com/v7/finance/chart/%s?period2=%d&period1=%d&interval=1m&indicators=quote&includeTimestamps=true&includePrePost=true&events=div%7Csplit%7Cearn&corsDomain=finance.yahoo.com"
 
 	return fmt.Sprintf(pattern, code, end.Unix(), start.Unix())
+}
+
+//	当前日期：获取当天0点的时间，跨天的返回前一天
+func getCurrentDate(endHour int) time.Time {
+
+	now := time.Now()
+	today := now.Add(-time.Hour * time.Duration(endHour))
+	return today.Truncate(time.Hour * 24)
 }
