26c187c15fcc24b86793a53b92d8a0abff8a84ba
nzai <nzai@163.com>
2015-07-19 23:47:49 +0800

    简化雅虎分时数据的抓取，以每一只股票为单位，分别抓取历史数据和今日数据。历史数据抓取最近60天的数据（雅虎财经所限），今日数据采用定时器循环，每天0点抓取前一天数据。

--- crawl/yahoo.go
+++ crawl/yahoo.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -12,13 +13,16 @@ import (
 )
 
 const (
-	gccount    = 16
-	retryCount = 5
-	retryDelay = 10 * time.Minute
+	gccount           = 16
+	retryCount        = 5
+	retryDelay        = 10 * time.Minute
+	configKeyTimeZone = "timezone"
+	//	雅虎财经的历史分时数据没有超过60天的
+	lastestDays = 60
 )
 
 //	抓取雅虎今日数据
-func yahooToday(market string, endHour int) error {
+func marketAll(market string) error {
 	//	股票编码
 	codes := config.GetArray(market, "codes")
 	if len(codes) == 0 {
@@ -30,22 +34,15 @@ func yahooToday(market string, endHour int) error {
 
 	for _, code := range codes {
 		//	并发抓取
-		go func(c string, h int) {
-
-			for try := 0; try < retryCount; try++ {
-				//	更新每只股票的历史
-				err := yahooTodayStock(c, h)
-				if err == nil {
-					break
-				}
-
+		go func(c string) {
+			err := stockAll(market, c)
+			if err != nil {
 				log.Fatal(err)
-				time.Sleep(retryDelay)
 			}
 
 			<-chanSend
 			chanReceive <- 1
-		}(code, endHour)
+		}(code)
 
 		chanSend <- 1
 	}
@@ -61,39 +58,116 @@ func yahooToday(market string, endHour int) error {
 	return nil
 }
 
-//	抓取雅虎今日股票数据
-func yahooTodayStock(code string, endHour int) error {
+//	抓取股票所有数据
+func stockAll(market, code string) error {
 
-	//	抓取数据
-	raw, err := io.GetString(yahooQueryUrl(code, endHour))
-	if err != nil {
-		return err
+	location := time.Local
+	timezone := config.GetString(market, configKeyTimeZone, "")
+	if timezone != "" {
+		loc, err := time.LoadLocation(timezone)
+		if err != nil {
+			return err
+		}
+		location = loc
+	}
+	now := time.Now().In(location)
+	today := now.Truncate(time.Hour * 24)
+
+	//	定时器今天
+	go func() {
+		ticker := time.NewTicker(time.Hour * 24)
+		log.Printf("已启动%s的定时抓取任务", code)
+		for _ = range ticker.C {
+			now = time.Now().In(location)
+			today = now.Truncate(time.Hour * 24)
+
+			err := stockToday(code, today)
+			if err != nil {
+				log.Fatalf("抓取%s在%s的数据出错:%v", code, today.Format("20060102"), err)
+			}
+		}
+	}()
+
+	//	历史
+	return stockHistory(code, today)
+}
+
+//	抓取今天
+func stockToday(code string, today time.Time) error {
+	log.Printf("%s在%s分时数据抓取任务-开始", code, today.Format("20060102"))
+	//	保存原始数据
+	day := today.Add(-time.Hour * 24)
+	for try := 0; try < retryCount; try++ {
+		//	抓取数据
+		err := thatDay(code, day)
+		if err != nil {
+			log.Fatalf("[%d]抓取%s在%s的数据出错:%v", try, code, today.Format("20060102"), err)
+			time.Sleep(retryDelay)
+		}
+
+		log.Printf("%s在%s分时数据抓取任务-结束", code, today.Format("20060102"))
+		return nil
 	}
 
-	//	保存原始数据
-	dataDir := config.GetDataDir()
+	return errors.New(fmt.Sprintf("%s在%s分时数据抓取任务失败", code, today.Format("20060102")))
+}
 
-	fileName := fmt.Sprintf("%s_raw.txt", getCurrentDate(endHour).Format("20060102"))
+//	抓取历史
+func stockHistory(code string, today time.Time) error {
+	log.Printf("%s历史分时数据抓取任务-开始", code)
+	//	保存原始数据
+	day := today.Add(-time.Hour * 24)
+	for index := 0; index < lastestDays; index++ {
+
+		for try := 0; try < retryCount; try++ {
+			//	抓取数据
+			err := thatDay(code, day)
+			if err == nil {
+				//	往前一天递推
+				day = day.Add(-time.Hour * 24)
+				break
+			} else {
+				log.Fatalf("[%d]抓取%s在%s的数据出错:%v", try, code, today.Format("20060102"), err)
+				time.Sleep(retryDelay)
+			}
+		}
+	}
+	log.Printf("%s历史分时数据抓取任务-结束", code)
+	return nil
+}
+
+//	抓取某一天
+func thatDay(code string, day time.Time) error {
+
+	//	文件保存路径
+	dataDir := config.GetDataDir()
+	fileName := fmt.Sprintf("%s_raw.txt", day.Format("20060102"))
 	filePath := filepath.Join(dataDir, code, fileName)
 
-	return io.WriteString(filePath, raw)
+	//	如果文件已存在就忽略
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		//	如果不存在就抓取并保存
+		start := day.Truncate(time.Hour * 24)
+		end := start.Add(time.Hour * 24)
+		html, err := peroid(code, start, end)
+		if err != nil {
+			return err
+		}
+
+		//	写入文件
+		return io.WriteString(filePath, html)
+	}
+
+	return nil
 }
 
-//	构造雅虎查询url
-func yahooQueryUrl(code string, endHour int) string {
-
-	start := getCurrentDate(endHour)
-	end := start.Add(time.Hour * 24)
+//	抓取一段时间
+func peroid(code string, start, end time.Time) (string, error) {
 
 	pattern := "https://finance-yql.media.yahoo.com/v7/finance/chart/%s?period2=%d&period1=%d&interval=1m&indicators=quote&includeTimestamps=true&includePrePost=true&events=div%7Csplit%7Cearn&corsDomain=finance.yahoo.com"
+	url := fmt.Sprintf(pattern, code, end.Unix(), start.Unix())
 
-	return fmt.Sprintf(pattern, code, end.Unix(), start.Unix())
-}
-
-//	当前日期：获取当天0点的时间，跨天的返回前一天
-func getCurrentDate(endHour int) time.Time {
-
-	now := time.Now().UTC()
-	today := now.Add(-time.Hour * time.Duration(endHour))
-	return today.Truncate(time.Hour * 24)
+	//	抓取数据
+	return io.GetString(url)
 }
