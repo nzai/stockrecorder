67a07b53922cd96334e4a4857bae9d54e406c618
nzai <nzai@163.com>
2015-07-24 02:00:12 +0800

    增加上市公司的抓取任务

--- task/task.go
+++ task/task.go
@@ -4,11 +4,19 @@ import (
 	"log"
 
 	"github.com/nzai/stockrecorder/crawl"
+	"github.com/nzai/stockrecorder/market"
 )
 
 //	启动任务
 func StartTasks() error {
 	log.Print("启动任务")
+
+	go func() {
+		err := market.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	//	启动抓取任务
 	go crawl.Start()
