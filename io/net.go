939e84d8bb8013ee139cedc47525fa9358ade5e4
nzai <nzai@16.3com>
2015-08-13 02:13:58 +0800

    增加包含referer的Get方法

--- io/net.go
+++ io/net.go
@@ -7,11 +7,27 @@ import (
 
 //	发送GET请求并返回结果字符串
 func GetString(url string) (string, error) {
+	return GetStringReferer(url, "")
+}
 
-	response, err := http.Get(url)
+//	发送GET请求并返回结果字符串(带Referer)
+func GetStringReferer(url, referer string) (string, error) {
+	
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return "", err
 	}
+	
+	if referer != "" {
+		request.Header.Set("Referer", referer)
+	}
+	
+	client := &http.Client{}	
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	
 	defer response.Body.Close()
 
 	buffer, err := ioutil.ReadAll(response.Body)
