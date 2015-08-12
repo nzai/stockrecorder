c8f4595f722a1d0b4ddea83c35367c0ec3b65e4e
nzai <nzai@16.3com>
2015-08-13 02:13:16 +0800

    彻底移除配置文件读取的逻辑

--- config/config.go
+++ config/config.go
@@ -3,35 +3,21 @@ package config
 import (
 	"os"
 	"path/filepath"
-
-	"github.com/Unknwon/goconfig"
 )
 
 const (
 	dataDirName = "data"
 )
 
-type Config struct {
-	filename   string
-	configFile *goconfig.ConfigFile
-}
-
-var configInstance *goconfig.ConfigFile
+var rootDir string
 var dataDir string
 
 //	设置配置文件
-func SetConfigFile(filePath string) error {
+func SetRootDir(root string) error {
 
-	configFile, err := goconfig.LoadConfigFile(filePath)
-	if err != nil {
-		return err
-	}
-
-	configInstance = configFile
-
-	rootDir := filepath.Dir(filePath)
+	rootDir = root
 	dataDir = filepath.Join(rootDir, dataDirName)
-	_, err = os.Stat(dataDir)
+	_, err := os.Stat(dataDir)
 	if os.IsNotExist(err) {
 		err = os.Mkdir(dataDir, 0x644)
 		if err != nil {
@@ -40,16 +26,6 @@ func SetConfigFile(filePath string) error {
 	}
 
 	return nil
-}
-
-//	获取字符串
-func GetString(section, key, defaultValue string) string {
-	return configInstance.MustValue(section, key, defaultValue)
-}
-
-//	获取字符串数组
-func GetArray(section, key string) []string {
-	return configInstance.MustValueArray(section, key, ",")
 }
 
 //	获取数据保存目录
