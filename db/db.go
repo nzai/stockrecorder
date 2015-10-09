026440452a0c81e28cb23833f8b071bd603f438f
nzai <nzai@16.3com>
2015-10-10 01:19:30 +0800

    MongoDB连接管理

--- db/db.go
+++ db/db.go
@@ -1,2 +1,22 @@
 package db
 
+import (
+	"gopkg.in/mgo.v2"
+	"github.com/nzai/stockrecorder/config"
+)
+
+var startSession *mgo.Session = nil
+
+//	获取数据库连接
+func Get() (*mgo.Session, error) {
+	if startSession == nil {
+		session, err := mgo.Dial(config.Get().MongoUrl)
+		if err != nil {
+			return nil, err
+		}
+		
+		startSession = session
+	}
+	
+	return startSession.Clone(), nil
+}
