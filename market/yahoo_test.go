b58653824fa82ac85f984fc36f3fd00b1dc0d1b4
nzai <nzai@16.3com>
2015-10-10 01:21:01 +0800

    根据错误日志重新编写了测试用例

--- market/yahoo_test.go
+++ market/yahoo_test.go
@@ -4,27 +4,47 @@ import (
 	"testing"
 	"time"
 
+	"log"
+
+	"github.com/nzai/stockrecorder/config"
+	"github.com/nzai/stockrecorder/db"
 	"github.com/nzai/stockrecorder/io"
 )
 
+func init() {
+
+	//	读取配置文件
+	err := config.SetRootDir(`c:\gohome\src\github.com\nzai\stockrecorder\`)
+	if err != nil {
+		log.Fatal("读取配置文件错误: ", err)
+		return
+	}
+}
+
 func TestParse60(t *testing.T) {
 
-	buffer, err := io.ReadAllBytes(`c:\data\America\AAPL\20151001_raw.txt`)
+	buffer, err := io.ReadAllBytes(`c:\data\America\ACHN\20151001_raw.txt`)
 	if err != nil {
 		t.Errorf("读取文件失败:%s", err.Error())
 	}
 
-	ar, err := ParseDailyYahooJson("America", "AAPL", time.Now(), buffer)
+	dd, _ := time.Parse("20060102", "20151001")
+	ar, err := parseDailyYahooJson("America", "AAPL", dd, buffer)
 	if err != nil {
 		t.Errorf("解析失败:%s", err.Error())
 	}
-	
+
 	if ar.DailyResult.Error {
 		t.Errorf("解析错误:%s", ar.DailyResult.Message)
 	}
-	
+
 	t.Log(ar)
 
-	var u1 int64 = 1443743880
+	err = db.DailySave(ar)
+	if err != nil {
+		t.Errorf("保存失败:%s", err.Error())
+	}
+
+	var u1 int64 = 14437023600
 	t.Logf("%d is %s", u1, time.Unix(u1, 0).Format("2006-01-02 15:04:05"))
 }
