eb8bfd9cd72e7667da324f4859b044c1aecdab02
nzai <nzai@16.3com>
2015-11-06 00:51:33 +0800

    查询前的代码转为大写

--- market/query.go
+++ market/query.go
@@ -2,6 +2,7 @@ package market
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -13,19 +14,21 @@ func QueryPeroid60(market, code string, start, end time.Time) ([]Peroid60, error
 		return nil, fmt.Errorf("[Query]\t未能找到市场%s", market)
 	}
 
+	_code := strings.ToUpper(code)
+
 	peroids := make([]Peroid60, 0)
 	for date := start; end == date || end.After(date); date = date.Add(time.Hour * 24) {
 
-		if !isExists(_market, code, date, regularSuffix) {
+		if !isExists(_market, _code, date, regularSuffix) {
 			continue
 		}
-		
+
 		//	读取
-		dayPeroids, err := loadPeroid60(_market, code, date)
+		dayPeroids, err := loadPeroid60(_market, _code, date)
 		if err != nil {
 			return nil, err
 		}
-		
+
 		if len(dayPeroids) > 0 {
 			peroids = append(peroids, dayPeroids...)
 		}
