dc96a6dac3b79cd69f7ec55aafd8d2325ce1cb2d
peter.yang <peteryang@yunio.com>
2018-01-02 18:58:50 +0800

    [feature]更新香港证交所股本证券清单的获取方式

--- market/hongkong.go
+++ market/hongkong.go
@@ -1,12 +1,11 @@
 package market
 
 import (
-	"bytes"
-	"sort"
-	"strings"
-
-	"github.com/360EntSecGroup-Skylar/excelize"
+	"errors"
+	"fmt"
 	"github.com/nzai/go-utility/net"
+	"regexp"
+	"sort"
 )
 
 // HongKong 香港证券市场
@@ -25,14 +24,19 @@ func (m HongKong) Timezone() string {
 // Companies 上市公司
 func (m HongKong) Companies() ([]Company, error) {
 
-	url := "http://sc.hkex.com.hk/TuniS/www.hkex.com.hk/chi/services/trading/securities/securitieslists/ListOfSecurities_c.xlsx"
+	url := "http://www.hkex.com.hk/Market-Data/Securities-Prices/Equities?sc_lang=zh-HK"
 
-	buffer, err := net.DownloadBufferRetry(url, retryTimes, retryIntervalSeconds)
+	body, err := net.DownloadStringRetry(url, retryTimes, retryIntervalSeconds)
 	if err != nil {
 		return nil, err
 	}
 
-	companies, err := m.parseXlsx(buffer)
+	token, err := m.getToken(body)
+	if err != nil {
+		return nil, err
+	}
+
+	companies, err := m.queryCompanies(token)
 	if err != nil {
 		return nil, err
 	}
@@ -43,26 +47,35 @@ func (m HongKong) Companies() ([]Company, error) {
 	return companies, nil
 }
 
-//	解析香港证券交易所上市公司
-func (m HongKong) parseXlsx(buffer []byte) ([]Company, error) {
+// getToken 获取访问api的token
+func (m HongKong) getToken(body string) (string, error) {
 
-	file, err := excelize.OpenReader(bytes.NewReader(buffer))
+	regex := regexp.MustCompile(`\"Base64-AES-Encrypted-Token\";\s*?return \"([^\"]+?)\";`)
+
+	matches := regex.FindStringSubmatch(body)
+	if len(matches) != 2 {
+		return "", errors.New("获取token失败")
+	}
+
+	return matches[1], nil
+}
+
+//	解析香港证券交易所上市公司
+func (m HongKong) queryCompanies(token string) ([]Company, error) {
+
+	url := fmt.Sprintf("https://www1.hkex.com.hk/hkexwidget/data/getequityfilter?lang=chi&sort=5&order=0&all=1&token=%s", token)
+
+	body, err := net.DownloadStringRetry(url, retryTimes, retryIntervalSeconds)
 	if err != nil {
 		return nil, err
 	}
 
-	rows := file.GetRows("ListOfSecuritites")
+	regex := regexp.MustCompile(`\"ric\":\"(\d{2,5})\.HK\"\S+?\"nm\":\"([^\"]+)\"`)
+	group := regex.FindAllStringSubmatch(body, -1)
+
 	var companies []Company
-	for _, values := range rows {
-
-		if strings.Trim(values[0], " ") == "" {
-			continue
-		}
-
-		companies = append(companies, Company{
-			Code: values[0],
-			Name: values[1],
-		})
+	for _, section := range group {
+		companies = append(companies, Company{Code: section[1], Name: section[2]})
 	}
 
 	return companies, nil
